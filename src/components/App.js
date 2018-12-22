9340ed34a436f69020724a80fe466d8de7c49b69
Gurveer Dhindsa <gurveerdhindsa@hotmail.com>
2018-12-21 21:10:59 -0500

    Forked electron-react boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../assets/css/App.css';
+import React, { Component } from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Main content</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
