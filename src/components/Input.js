b54f94cb7768c08be6390290c77d5f9c9285c3cc
Gurveer Dhindsa <gurveerdhindsa@hotmail.com>
2018-12-24 11:37:19 -0500

    Debounce user input, validate youtube URL + styling

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react';
 import '../assets/css/Input.css';
 
-const Header = props => (
-    <div>
-        <input className="input" placeholder="Enter a youtube URL" type="text"></input>
-    </div>
-);
+class Input extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleInput = this.handleInput.bind(this);
+        this.state = {
+            value: '',
+            timeout: 0,
+            urlValid: ''
+        }
+    }
 
-export default Header
+    handleInput(e) {
+        if (this.state.timeout) {
+            clearTimeout(this.state.timeout);
+        }
+
+        this.setState({
+            value: e.target.value,
+            timeout: setTimeout(() => {
+                this.validateURL(this.state.value);
+            }, 800)
+        });
+
+
+    }
+
+    validateURL(url) {
+
+        if (!url) {
+            console.log('No youtube url')
+            this.setState({
+                urlValid: ''
+            });
+        }
+        else {
+            var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
+            var match = url.match(regExp);
+            if (match && match[2].length == 11) {
+                console.log('Valid youtube url')
+                this.setState({
+                    urlValid: 'valid'
+                });
+            }
+            else {
+                console.log('Invalid youtube url')
+                this.setState({
+                    urlValid: 'invalid'
+                });
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <input className={"input " + this.state.urlValid}
+                       placeholder="Enter a youtube URL"
+                       type="text"
+                       onChange={(e) => {this.handleInput(e)}} />
+            </div>
+        );
+    }
+}
+
+export default Input
